12617095d1530becee261ac336f4f40af8b1b083
TEC <git@tecosaur.net>
2025-05-10 17:21:29 +0800

    Add orphan detection and entry purging support

--- src/Trash.jl
+++ src/Trash.jl
@@ -8,7 +8,7 @@ using Dates
 export trash, untrash, TrashFile
 
 @static if VERSION >= v"1.11"
-    eval(Expr(:public, :trashdir, :trashes, :list, :search, :empty))
+    eval(Expr(:public, :trashdir, :trashes, :list, :search, :orphans, :purge, :empty))
 end
 
 include("generic.jl")
@@ -30,6 +30,9 @@ precompile(untrash, (String,))
 precompile(list, ())
 precompile(list, (String,))
 precompile(search, (String,))
+precompile(orphans, ())
+precompile(orphans, (String,))
+precompile(purge, (TrashFile,))
 precompile(trashdir, ())
 precompile(trashdir, (String,))
 precompile(trashes, ())
@@ -42,14 +45,20 @@ precompile(empty, (String,))
 Cross-platform file trashing library.
 
 Move files and directories to your system's trash rather than deleting
-them outright, and manage trashed items programmatically:
+them outright, and manage trashed items programmatically.
 
-- `trash(path)`: send a file or folder to Trash.
+# API
+
+- `TrashFile`: a representation of a trashed file or folder
+- `trash(path)`: send a file or folder to Trash. Provides a `TrashFile` object that
+  can be used to retrieve (`untrash`) or purge the item later.
+- `untrash(entry; ...)`: restore a trashed item, from either a `TrashFile` or or a path to a trashed item.
+  This operates on both `TrashFile` entries, as well as an original path to a trashed item.
+- `purge(entry::TrashFile)`: permanently delete a trashed item.
 - `trashdir(path)`: find the directory of the trash that would be used for a certain path.
-- `list([trashdir])`: list all trashed items as a `Vector{TrashFile}` (with original paths and deletion timestamps).
-- `untrash(entry; pick=:only, force=false, rm=false)`: restore one trashed item
-  (choose oldest/newest on duplicates; handle conflicts). This operates on both `TrashFile` entries, and
-  the original path to a file or folder that has been deleted.
+- `list([trashdir])`: list all trashed items (as a `Vector{TrashFile}`, including original paths and deletion timestamps).
+- `search(path)`: look for a path in the trash.
+- `orphans([trashdir])`: list all items that have lost either the requisite data or metadata.
 - `empty([trashdir])`: empty the trash directory (permanently delete all items in it).
 
 # Examples
